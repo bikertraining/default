bd3a07eb8c539d7600bbf2982b7ce9a631f2f5e7
Sammie S. Taunton <diemuzi@gmail.com>
2025-10-23 08:48:48 -0500

    Max Length 255

--- util/form/templatetags/get_form_textarea.py
+++ util/form/templatetags/get_form_textarea.py
@@ -5,13 +5,13 @@ register = template.Library()
 
 
 @register.simple_tag
-def get_form_textarea(label, name, error_text=None, help_text=None, required=True, rows='3', value=None):
+def get_form_textarea(label, name, error_text=None, help_text=None, required=True, rows='3', maxlength='255', value=None):
     if error_text is None:
         error_text = {}
 
     context = (f'<div class="TextInput{" required" if required else ""}">'
                f'<label class="fw-bold form-label" for={name}>{label}</label>'
-               f'<textarea class="form-control{" is-invalid" if error_text else ""}" id="{name}" name="{name}" placeholder="{label}" rows="{rows}">{value}</textarea>')
+               f'<textarea class="form-control{" is-invalid" if error_text else ""}" id="{name}" name="{name}" maxlength="{maxlength}" placeholder="{label}" rows="{rows}">{value}</textarea>')
 
     if help_text is not None:
         context += f'<div class="form-text">{help_text}</div>'
