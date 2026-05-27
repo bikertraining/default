2fa71eeefaf4dae17f8be495562385978e1b1e5c
Sammie S. Taunton <diemuzi@gmail.com>
2026-05-27 07:33:27 -0500

    Redirect old urls

--- client/courses/urls.py
+++ client/courses/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import RedirectView
 
 from client.courses import views
 
@@ -7,5 +8,30 @@ urlpatterns = [
         '',
         views.Index.as_view(),
         name='client-courses-index'
+    ),
+    path(
+        '3wbrc',
+        RedirectView.as_view(url='/courses/', permanent=True),
+        name='client-courses-3wbrc'
+    ),
+    path(
+        'brc',
+        RedirectView.as_view(url='/courses/', permanent=True),
+        name='client-courses-brc'
+    ),
+    path(
+        'src',
+        RedirectView.as_view(url='/courses/', permanent=True),
+        name='client-courses-src'
+    ),
+    path(
+        'kickstart',
+        RedirectView.as_view(url='/courses/', permanent=True),
+        name='client-courses-kickstart'
+    ),
+    path(
+        'private',
+        RedirectView.as_view(url='/courses/', permanent=True),
+        name='client-courses-private'
     ),
 ]
